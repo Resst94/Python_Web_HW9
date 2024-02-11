d81bb96d533d045ce40ec500624dfa2812f0cd8e
Maksym Melnyk <145143194+Resst94@users.noreply.github.com>
2024-02-11 13:13:36 +0100

    Add files via upload

---
+++ models.py
@@ -0,0 +1,24 @@
+from mongoengine import Document, StringField, ReferenceField, ListField, CASCADE
+from mongoengine import connect
+
+connect(
+    db="web20",
+    host="mongodb+srv://userweb:kCMUpFTzrjqCMW56@cluster1.8x3jujn.mongodb.net/?retryWrites=true&w=majority",
+    tls=True,
+    tlsInsecure=True,
+)
+
+
+class Author(Document):
+    fullname = StringField(max_length=150, required=True, unique=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField(max_length=100)
+    description = StringField()
+    meta = {"collection": 'authors'}
+
+
+class Quote(Document):
+    tags = ListField(StringField(max_length=230))
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    quote = StringField()
+    meta = {"collection": 'quotes'}
